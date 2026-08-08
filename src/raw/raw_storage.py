b2d6665e510fe402b205502dad7e81bc5f1e74fc
chrisryan2210@gmail.com <chrisryan2210@gmail.com>
2026-08-08 15:27:10 +0000

    fixed raw storage bug - now working with scheduled job

--- src/raw/raw_storage.py
+++ src/raw/raw_storage.py
@@ -1,6 +1,6 @@
 import json
 from datetime import datetime
-from src.config.get_api_info import get_raw_storage_location
+from src.utils.get_api_info import get_raw_storage_location
 
 def save_raw_json(data, file_type):
 
